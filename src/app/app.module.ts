a6265ada5caf7a3147edae6bc24849edba712de5
Lfelipe-Freitas <lfelipe.dfreitas@gmail.com>
2022-10-13 17:52:27 -0300

    Criação de 2 componentes teste

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,23 @@
+import { ProdutoComponent } from './produto/produto.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CarrosselComponent } from './carrossel/carrossel.component';
 
 @NgModule({
+  /*declarations servem para registrar, components, pipes, diretivas e outras estruturas.*/
   declarations: [
-    AppComponent
+    AppComponent,
+    ProdutoComponent,
+    CarrosselComponent
   ],
+  /*Imports servem para registrar outros módulos dentro de outro módulo.
+  EXCLUSIVAMENTE PARA OUTROS MÓDULOS*/
   imports: [
     BrowserModule
   ],
+  /*Providers servem para registrar serviços e interceptadores HTTP*/
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/produto/produto.component.ts
@@ -0,0 +1,11 @@
+import { Component} from "@angular/core";
+
+/*Todo componente do Angular é representado por uma classe */
+@Component({
+  selector: "card-produto",
+  templateUrl: "./produto.component.html",
+  styleUrls: ["./produto.component.css"]
+})
+export class ProdutoComponent {
+
+}
